ca154e3294e5b28cba8d34eea2fc7e9cdb68365e
Becky <beckyhaystar@gmail.com>
2022-09-07 15:02:47 +0100

    Fix styling and size of the pie

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { DragDropContext } from "react-beautiful-dnd";
+import { DragDropContext, Droppable } from "react-beautiful-dnd";
 import ChosenWords from "./components/ChosenWords";
 import RemoveWordArea from "./components/RemoveWordArea";
 import ChangeNumWordsButton from "./components/ChangeNumWordsButton";
@@ -25,12 +25,14 @@ class App extends Component {
 
   async toggleTimer(currentTimerRunning) {
     var newTimerRunning = !currentTimerRunning;
-    console.log(currentTimerRunning);
+
+    if (newTimerRunning && this.state.timeLeftSec == 0) {
+      this.resetTimer();
+    }
     await this.setState({ timerRunning: newTimerRunning });
     if (newTimerRunning) {
       this.runTimer();
     }
-    console.log(this.state.timerRunning);
   }
 
   async runTimer() {
@@ -53,7 +55,10 @@ class App extends Component {
   }
 
   resetTimer = () => {
-    this.setState({ timeLeftSec: this.state.totalTimeSec });
+    this.setState({
+      timerRunning: false,
+      timeLeftSec: this.state.totalTimeSec,
+    });
   };
 
   setTotalTime = (seconds) => {
@@ -79,6 +84,21 @@ class App extends Component {
         : "";
     var sDisplay = m > 0 ? String(s).padStart(2, "0") : s;
     return hDisplay + mDisplay + sDisplay;
+  };
+
+  onDragStart = () => {
+    this.setState({ isDragging: true });
+  };
+
+  onDragEnd = (result) => {
+    const { destination, source, draggableId } = result;
+
+    if (!destination) {
+      return;
+    }
+
+    if (destination.droppableId === "clock-area") {
+    }
   };
 
   changeNumWords = (diff) => {
@@ -192,216 +212,166 @@ class App extends Component {
     const timeDisplay = this.secondsToHms(this.state.timeLeftSec);
     return (
       <div className="App">
-        <div className="h-full bg-indigo-100 p-8">
+        <div className="bg-indigo-100 p-8">
           <h1 className="text-indigo-900 title-font text-2xl">Visual Timer</h1>
         </div>
-        <section className="text-gray-600 body-font">
-          <div className="container md:px-32 sm:px-10 px-2 pb-24 mx-auto">
-            <div className="text-center mb-5 mt-5">
-              <div>
-                <div className="h-96 w-96 m-auto">
-                  <div className="flex flex-row space-around align-center justify-center h-full w-full">
-                    <div className="pie relative w-full h-full rounded-full bg-red-300 border-2 border-red-700">
-                      <div
-                        className="ticker absolute left-0 w-1/2 h-full rounded-l-full origin-right bg-red-700"
-                        style={{
-                          transform: `rotate(${-3.6 * percentage + 180}deg)`,
-                          zIndex: 0,
-                        }}
-                      ></div>
-                      <div
-                        className="mask absolute left-0 w-1/2 h-full rounded-l-full origin-right bg-red-700"
-                        style={{
-                          backgroundColor: `${
-                            percentage > 50 ? "" : "#fca5a5"
-                          }`,
-                          transform: `rotate(-${percentage > 50 ? 0 : 180}deg)`,
-                          zIndex: 1,
-                        }}
-                      ></div>
-                    </div>
+        <section className="container text-gray-600 body-font my-5 mx-auto">
+          {/* <div className="h-96 w-96 m-auto"> */}
+          {/* <div className="flex flex-row space-around align-center justify-center h-full w-full">
+                  <div className="pie relative w-full h-full rounded-full bg-red-300 border-2 border-red-700">
+                    <svg width="100" height="100">
+                      <circle
+                        cx="50"
+                        cy="50"
+                        r="40"
+                        stroke="green"
+                        strokeWidth="4"
+                        fill="yellow"
+                      ></circle>
+                    </svg>
+
+                    <DragDropContext
+                      onDragStart={this.onDragStart}
+                      onDragEnd={this.onDragEnd}
+                    >
+                      <Droppable
+                        droppableId="ticker-drag"
+                        direction="horizontal"
+                      >
+                        {(provided) => (
+                          <div className="absolute l-1/2 w-1 h-full bg-blue-900 z-10"></div>
+                        )}
+                      </Droppable>
+                      <Droppable
+                        droppableId="clock-area"
+                        direction="horizontal"
+                      >
+                        {(provided) => (
+                          <div
+                            className="mask absolute left-0 w-1/2 h-full rounded-l-full origin-right bg-red-300"
+                            style={{
+                              backgroundColor: `${
+                                percentage > 50 ? "" : "#fca5a5"
+                              }`,
+                              transform: `rotate(-${
+                                percentage > 50 ? 0 : 180
+                              }deg)`,
+                              zIndex: 1,
+                            }}
+                          ></div>
+                        )}
+                      </Droppable>
+                    </DragDropContext>
                   </div>
-                </div>
-                <div className="flex flex-row space-around align-center justify-center">
-                  {timeDisplay}
-                </div>
-                <div className="flex flex-row space-around align-center justify-center">
+                </div> */}
+          {/* </div> */}
+          <div
+            className="mx-auto"
+            onClick={() =>
+              this.state.timeLeftSec == 0
+                ? this.resetTimer()
+                : this.toggleTimer(this.state.timerRunning)
+            }
+          >
+            <div className="flex flex-row space-around align-center justify-center mx-auto">
+              <div
+                className="pie relative rounded-full bg-red-300 border-2 border-red-700 w-3/4 max-w-md max-h-md h-0"
+                style={{
+                  paddingBottom: "min(75%, 28rem)",
+                }}
+              >
+                <div
+                  className="ticker absolute left-0 w-1/2 h-full rounded-l-full origin-right bg-red-700"
+                  style={{
+                    transform: `rotate(${-3.6 * percentage + 180}deg)`,
+                    zIndex: 0,
+                  }}
+                ></div>
+                <div
+                  className="mask absolute left-0 w-1/2 h-full rounded-l-full origin-right bg-red-700"
+                  style={{
+                    backgroundColor: `${percentage > 50 ? "" : "#fca5a5"}`,
+                    transform: `rotate(-${percentage > 50 ? 0 : 180}deg)`,
+                    zIndex: 1,
+                  }}
+                ></div>
+              </div>
+            </div>
+            <div className="flex flex-row space-around align-center justify-center">
+              {timeDisplay}
+            </div>
+            {/* <div className="flex flex-row space-around align-center justify-center">
                   custom time selector
                 </div>
                 <div className="flex flex-row space-around align-center justify-center">
                   options button for popup
-                </div>
-              </div>
+                </div> */}
+          </div>
 
-              <div>
+          {/* <div>
                 <div className="flex flex-row space-around align-center justify-center">
-                  option: colour buttons
+                  option: colour buttons, dark mode
                 </div>
-              </div>
+              </div> */}
 
-              <div>
-                <div className="flex flex-row space-around align-center justify-center">
-                  <button
-                    className="button bg-blue-100 p-2 m-4 rounded-md text-blue-900 font-bold border border-blue-300"
-                    onClick={() =>
-                      this.state.timeLeftSec == 0
-                        ? this.resetTimer()
-                        : this.toggleTimer(this.state.timerRunning)
-                    }
-                  >
-                    {this.state.timerRunning
-                      ? "Pause"
-                      : this.state.timeLeftSec == 0
-                      ? "Reset"
-                      : "Start"}
-                  </button>
-                </div>
-                <div className="flex flex-row space-around align-center justify-center">
-                  <button
+          <div className="w-full h-full">
+            <div className="flex flex-row space-around align-center justify-center">
+              <button
+                className="button bg-blue-100 p-2 m-4 rounded-md text-blue-900 font-bold border border-blue-300"
+                onClick={() => this.toggleTimer(this.state.timerRunning)}
+              >
+                {this.state.timerRunning ? "Pause" : "Start"}
+              </button>
+              <button
+                className="button bg-blue-100 p-2 m-4 rounded-md text-blue-900 font-bold border border-blue-300"
+                onClick={this.resetTimer}
+              >
+                Reset
+              </button>
+            </div>
+            <div className="flex flex-row space-around align-center justify-center">
+              {/* <button
                     className="m-1 p-3 rounded-md bg-blue-300 font-bold"
                     onClick={() => this.setTotalTime(300)}
                   >
                     5 min
-                  </button>
-                  <button
+                  </button> */}
+              {/* <button
                     className="m-1 p-3 rounded-md bg-blue-300 font-bold"
                     onClick={() => this.setTotalTime(600)}
                   >
                     10 min
-                  </button>
-                  <button
-                    className="m-1 p-3 rounded-md bg-blue-300 font-bold"
-                    onClick={() => this.setTotalTime(900)}
-                  >
-                    15 min
-                  </button>
-                  <button
-                    className="m-1 p-3 rounded-md bg-blue-300 font-bold"
-                    onClick={() => this.setTotalTime(1200)}
-                  >
-                    20 min
-                  </button>
-                  <button
-                    className="m-1 p-3 rounded-md bg-blue-300 font-bold"
-                    onClick={() => this.setTotalTime(1800)}
-                  >
-                    30 min
-                  </button>
-                  <button
+                  </button> */}
+              <button
+                className="m-1 p-3 rounded-md bg-blue-300 font-bold"
+                onClick={() => this.setTotalTime(900)}
+              >
+                15 min
+              </button>
+              <button
+                className="m-1 p-3 rounded-md bg-blue-300 font-bold"
+                onClick={() => this.setTotalTime(1200)}
+              >
+                20 min
+              </button>
+              <button
+                className="m-1 p-3 rounded-md bg-blue-300 font-bold"
+                onClick={() => this.setTotalTime(3600)}
+              >
+                1 hour
+              </button>
+              <button
+                className="m-1 p-3 rounded-md bg-blue-300 font-bold"
+                onClick={() => this.setTotalTime(9000)}
+              >
+                2h 30m
+              </button>
+              {/* <button
                     className="m-1 p-3 rounded-md bg-blue-300 font-bold"
                     onClick={() => this.setTotalTime(2100)}
                   >
                     45 min
-                  </button>
-                  <button
-                    className="m-1 p-3 rounded-md bg-blue-300 font-bold"
-                    onClick={() => this.setTotalTime(3600)}
-                  >
-                    1 hour
-                  </button>
-                </div>
-              </div>
-
-              <div className="flex flex-row flex-auto space-around align-center justify-center">
-                <div>
-                  <div className="flex flex-auto flex-row content-center">
-                    <ChangeNumWordsButton
-                      diff={-1}
-                      changeNumWords={this.changeNumWords}
-                    />
-                    <ChooseRandomWordsButton
-                      numWords={this.state.numWords}
-                      onClick={this.chooseRandomWords}
-                    />
-                    <ChangeNumWordsButton
-                      diff={1}
-                      changeNumWords={this.changeNumWords}
-                    />
-                  </div>
-                </div>
-              </div>
-              <div className="flex flex-row justify-center content-center text-base text-center leading-relaxed text-gray-400 pt-4">
-                <button
-                  className="flex flex-row border border-indigo-100 rounded-xl p-1 select-none focus:outline-none hover:bg-indigo-50"
-                  onClick={this.clearWords}
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-6 w-6 pl-1"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth={2}
-                      d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"
-                    />
-                  </svg>
-                  <p className="px-2">Clear</p>
-                </button>
-              </div>
-              <div className="border-double border-4 border-indigo-200 rounded-md p-3 mt-6 xl:mx-96 lg:mx-64 sm:mx-24 mx-12">
-                <p className="">
-                  Choose the number of words you'd like, then press the button
-                  to randomly generate. You can also manually select words
-                  below, and drag to rearrange/delete them.
-                </p>
-              </div>
-            </div>
-            <div className="flex flex-row justify-center content-center text-base leading-relaxed text-gray-400">
-              <button
-                className="flex flex-row border border-indigo-100 rounded-xl p-1 select-none focus:outline-none hover:bg-indigo-50 px-2 mx-2"
-                onClick={this.collapseCategories}
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5 mt-1"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M4.293 15.707a1 1 0 010-1.414l5-5a1 1 0 011.414 0l5 5a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414 0zm0-6a1 1 0 010-1.414l5-5a1 1 0 011.414 0l5 5a1 1 0 01-1.414 1.414L10 5.414 5.707 9.707a1 1 0 01-1.414 0z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-                <p className="px-2">Collapse all</p>
-              </button>
-              <button
-                className="flex flex-row border border-indigo-100 rounded-xl p-1 select-none focus:outline-none hover:bg-indigo-50 px-2 mx-2"
-                onClick={this.expandCategories}
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5 mt-1"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M15.707 4.293a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0l-5-5a1 1 0 011.414-1.414L10 8.586l4.293-4.293a1 1 0 011.414 0zm0 6a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0l-5-5a1 1 0 111.414-1.414L10 14.586l4.293-4.293a1 1 0 011.414 0z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-                <p className="px-2">Expand all</p>
-              </button>
-            </div>
-            <div className="container flex flex-wrap m-4 mx-auto text-center items-left justify-left">
-              {Object.keys(wordsData).map((category) => {
-                return (
-                  <Category
-                    key={`category-${category}`}
-                    category={category}
-                    words={wordsData[category]}
-                    onClickWord={this.onClickWord}
-                    selectedWords={this.state.selectedWords}
-                    collapse={this.state.categoriesCollapsed}
-                    expand={this.state.categoriesExpanded}
-                  />
-                );
-              })}
+                  </button> */}
             </div>
           </div>
         </section>
